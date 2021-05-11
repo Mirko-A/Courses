9c9ec28f651fdf9a2ea5772d8380a5bcf9f78714
Mirko Adzic <adzicmirko97@gmail.com>
2021-05-11 14:50:13 +0200

    Update SIS_VEZBA_4_RESENO.m

--- SIS/V4/SIS_VEZBA_4_RESENO.m
+++ SIS/V4/SIS_VEZBA_4_RESENO.m
@@ -10,11 +10,11 @@ H3_n=[1];
 H3_d=[1 4];
 H3=tf(H3_n,H3_d)
 %
-%pitati za cloop
+%
 [H2cloop_n, H2cloop_d]=cloop(H2_n, H2_d);
 [H1H2cloop_n, H1H2cloop_d]=parallel(H1_n, H1_d, H2cloop_n, H2cloop_d);
 [G_n, G_d]=feedback(H1H2cloop_n, H1H2cloop_d, H3_n, H3_d);
-G=tf(G_n, G_d);
+G=tf(G_n, G_d)
 figure(2)
 subplot(2,2,1)
 impulse(G)
